671d8f6196c6a1f6ad90f5f8da01ac476718be0c
Mark Burnett <mark.m.burnett@gmail.com>
2014-06-06 15:19:22 -0500

    travis-ci:  trying to debug missing web coverage report

--- tests/scripts/web_wrapper.py
+++ tests/scripts/web_wrapper.py
@@ -1,6 +1,7 @@
 import os
 import signal
 import subprocess
+import sys
 import time
 
 
@@ -9,10 +10,12 @@ LOOP_PERIOD = 0.05
 def webserver_path():
     return os.path.join(os.path.dirname(__file__), 'web_service.py')
 
+sys.stderr.write('Wrapping web server for coverage collection\n')
 child_process = subprocess.Popen(['coverage', 'run', webserver_path()])
 
 
 def term_handler(signum, frame):
+    sys.stderr.write('Sending SIGINT to wrapped coverage process')
     child_process.send_signal(signal.SIGINT)
 
     def kill_child(signum, frame):
@@ -20,7 +23,7 @@ def term_handler(signum, frame):
             child_process.kill()
 
     signal.signal(signal.SIGALRM, kill_child)
-    signal.alarm(4)
+    signal.alarm(10)
 
 signal.signal(signal.SIGINT, term_handler)
 signal.signal(signal.SIGTERM, term_handler)
