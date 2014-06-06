c93665c0cf377b99c89018c1c6fd4d827220b974
Ian Ferguson <iferguso@genome.wustl.edu>
2014-06-06 13:54:35 -0500

    Trying to get coverage for web server

--- ptero_shell_command_fork/api/wsgi.py
+++ ptero_shell_command_fork/api/wsgi.py
@@ -1,7 +1,10 @@
-from . import application
+from ptero_shell_command_fork.api import application
+import os
 
 app = application.create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PTERO_FORK_PORT', '5200'))
+    host = os.environ.get('PTERO_FORK_HOST', 'localhost')
+    app.run(port=port, host=host)
