e0247c0210e34a5baa3a59544be7e6812b744dc4
Ian Ferguson <iferguso@genome.wustl.edu>
2014-06-10 15:49:01 -0500

    Remove defaults for PTERO_FORK_* in tests

--- tests/scripts/web_service.py
+++ tests/scripts/web_service.py
@@ -1,8 +1,8 @@
 from ptero_shell_command_fork.api.wsgi import app
 import os
 
-port = int(os.environ.get('PTERO_FORK_PORT', '5200'))
-host = os.environ.get('PTERO_FORK_HOST', 'localhost')
+port = int(os.environ['PTERO_FORK_PORT'])
+host = os.environ['PTERO_FORK_HOST']
 
 
 app.run(host=host, port=port, debug=False, use_reloader=False)
