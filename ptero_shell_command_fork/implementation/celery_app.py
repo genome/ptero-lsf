5166454d134632a053456389bb3e3cde7a49280f
Mark Burnett <mark.m.burnett@gmail.com>
2014-09-13 12:35:00 -0500

    get broker_url from environment

--- ptero_shell_command_fork/implementation/celery_app.py
+++ ptero_shell_command_fork/implementation/celery_app.py
@@ -2,14 +2,13 @@ import celery
 import os
 
 
-_CONFIG_TO_UPDATE_FROM_ENVIRONMENT = [
-    'CELERY_RESULT_BACKEND',
-]
-
 app = celery.Celery('PTero-fork-celery',
         include='ptero_shell_command_fork.implementation.celery_tasks')
 
-conf_dict = {
+
+_DEFAULT_CELERY_CONFIG = {
+    'CELERY_BROKER_URL': 'amqp://localhost',
+    'CELERY_RESULT_BACKEND': 'redis://localhost',
     'CELERY_ACCEPT_CONTENT': ['json'],
     'CELERY_ACKS_LATE': True,
     'CELERY_RESULT_SERIALIZER': 'json',
@@ -17,7 +16,8 @@ conf_dict = {
     'CELERY_TRACK_STARTED': True,
     'CELERYD_PREFETCH_MULTIPLIER': 1,
 }
-for var in _CONFIG_TO_UPDATE_FROM_ENVIRONMENT:
+for var, default in _DEFAULT_CELERY_CONFIG.iteritems():
     if var in os.environ:
-        conf_dict[var] = os.environ[var]
-app.conf.update(**conf_dict)
+        app.conf[var] = os.environ[var]
+    else:
+        app.conf[var] = default
