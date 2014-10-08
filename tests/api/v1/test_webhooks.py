4aa84738e48d8e76cfe42094e3bec6c7a004093d
David Morton <davidlmorton@gmail.com>
2014-10-08 13:14:59 -0500

    Make command_line (in json) camelCase

--- tests/api/v1/test_webhooks.py
+++ tests/api/v1/test_webhooks.py
@@ -7,7 +7,7 @@ class TestWebhooks(BaseAPITest):
         callback_server = self.create_callback_server([200])
 
         post_response = self.post(self.jobs_url, {
-            'command_line': ['true'],
+            'commandLine': ['true'],
             'callbacks': {
                 'begun': callback_server.url,
             },
@@ -26,7 +26,7 @@ class TestWebhooks(BaseAPITest):
         callback_server = self.create_callback_server([200])
 
         post_response = self.post(self.jobs_url, {
-            'command_line': ['true'],
+            'commandLine': ['true'],
             'callbacks': {
                 'ended': callback_server.url,
             },
@@ -48,7 +48,7 @@ class TestWebhooks(BaseAPITest):
         callback_server = self.create_callback_server([200])
 
         post_response = self.post(self.jobs_url, {
-            'command_line': ['false'],
+            'commandLine': ['false'],
             'callbacks': {
                 'ended': callback_server.url,
             },
@@ -70,7 +70,7 @@ class TestWebhooks(BaseAPITest):
         callback_server = self.create_callback_server([200, 200])
 
         post_response = self.post(self.jobs_url, {
-            'command_line': ['true'],
+            'commandLine': ['true'],
             'callbacks': {
                 'begun': callback_server.url,
                 'ended': callback_server.url,
@@ -100,7 +100,7 @@ class TestWebhooks(BaseAPITest):
         }
 
         post_data = {
-            'command_line': ['/usr/bin/env'],
+            'commandLine': ['/usr/bin/env'],
             'environment': environment,
             'callbacks': {
                 'ended': callback_server.url,
@@ -121,7 +121,7 @@ class TestWebhooks(BaseAPITest):
         stdin = 'this is just some text'
 
         post_data = {
-            'command_line': ['cat'],
+            'commandLine': ['cat'],
             'stdin': stdin,
             'callbacks': {
                 'ended': callback_server.url,
