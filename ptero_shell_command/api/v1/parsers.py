341114641fb33f7aa1a07b2266f57be071e59f87
Michael J. Kiwala <michael.kiwala@gmail.com>
2014-10-22 09:52:05 -0500

    restore alphabetical order to list of parse arguments

--- ptero_shell_command/api/v1/parsers.py
+++ ptero_shell_command/api/v1/parsers.py
@@ -2,10 +2,10 @@ from flask.ext.restful import reqparse
 
 
 _job_post = reqparse.RequestParser()
+_job_post.add_argument('callbacks', type=dict)
 _job_post.add_argument('commandLine', type=list, dest='command_line')
 _job_post.add_argument('environment', type=dict)
 _job_post.add_argument('stdin', type=str)
-_job_post.add_argument('callbacks', type=dict)
 def get_job_post_data():
     data = _job_post.parse_args()
     return data
