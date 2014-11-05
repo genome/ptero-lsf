9111c3364345deec1671de962eb51d28d94770f2
Michael J. Kiwala <michael.kiwala@gmail.com>
2014-11-05 14:52:42 -0600

    extend job user test to also test for group
    It turned out that setting the user has the side-effect of also setting the
    groups, so no additional deliverable source code is needed.

--- tests/api/v1/test_fork_worker_running_as_root.py
+++ tests/api/v1/test_fork_worker_running_as_root.py
@@ -1,16 +1,18 @@
 from .base import BaseAPITest
 import os
+import re
 import unittest
 
 @unittest.skipIf(not os.environ.get('TEST_WITH_ROOT_WORKERS'),
     "not running fork worker as root")
 class TestForkWorkerRunningAsRoot(BaseAPITest):
-    def test_user_of_job(self):
+    def test_job_user_and_group(self):
         callback_server = self.create_callback_server([200])
 
         username = 'nobody'
+        primarygroup = 'nobody'
         self.post(self.jobs_url, {
-            'commandLine': ['whoami'],
+            'commandLine': ['id'],
             'username': username,
             'callbacks': {
                 'ended': callback_server.url,
@@ -18,6 +20,17 @@ class TestForkWorkerRunningAsRoot(BaseAPITest):
         })
 
         webhook_data = callback_server.stop()
-        actual_username = webhook_data[0]['stdout'].rstrip('\n')
+        id_result = webhook_data[0]['stdout']
+
+        actual_username = self._find_match(r"uid=\d+\((\w+)\)", id_result)
+        actual_primarygroup = self._find_match(r"gid=\d+\((\w+)\)", id_result)
 
         self.assertEqual(username, actual_username)
+        self.assertEqual(primarygroup, actual_username)
+
+    def _find_match(self, regexp, target):
+        match = re.search(regexp, target)
+        if match:
+            return match.group(1)
+        else:
+            return ''
