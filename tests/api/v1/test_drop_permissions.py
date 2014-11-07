e13eb8a281221a8cab3cb292b60dfb80c7819deb
Michael J. Kiwala <michael.kiwala@gmail.com>
2014-11-07 14:52:22 -0600

    combine all tests of setting uid/gid into one test class
    The were spread accross 3 files that:
        * ran only when the fork worker was root
        * ran only when the fork worker was NOT root
        * always ran, regardless of fork worker user
    
    I mostly preserved this behavior of test skipping, decorating individual test
    methods instead of decorating the entire test class.  The test that jobs can
    never run as root moved from a root-fork-worker-only test, to a regular
    run-all-of-the-time test.

--- tests/api/v1/test_drop_permissions.py
+++ tests/api/v1/test_drop_permissions.py
@@ -3,9 +3,7 @@ import os
 import re
 import unittest
 
-@unittest.skipIf(not os.environ.get('TEST_WITH_ROOT_WORKERS'),
-    "not running fork worker as root")
-class TestForkWorkerRunningAsRoot(BaseAPITest):
+class TestDropPermissions(BaseAPITest):
     def test_running_job_as_root_should_fail(self):
         callback_server = self.create_callback_server([200])
 
@@ -28,7 +26,8 @@ class TestForkWorkerRunningAsRoot(BaseAPITest):
         ]
         self.assertEqual(expected_data, webhook_data)
 
-
+    @unittest.skipIf(not os.environ.get('TEST_WITH_ROOT_WORKERS'),
+        "not running fork worker as root")
     def test_job_user_and_group(self):
         callback_server = self.create_callback_server([200])
 
@@ -50,6 +49,52 @@ class TestForkWorkerRunningAsRoot(BaseAPITest):
 
         self.assertEqual(user, actual_user)
         self.assertEqual(primarygroup, actual_user)
+
+    @unittest.skipIf(os.environ.get('TEST_WITH_ROOT_WORKERS'),
+        "running fork worker as root")
+    def test_user_of_job(self):
+        callback_server = self.create_callback_server([200])
+
+        user = 'nobody'
+        post_response = self.post(self.jobs_url, {
+            'commandLine': ['whoami'],
+            'user': user,
+            'callbacks': {
+                'error': callback_server.url,
+            },
+        })
+
+        webhook_data = callback_server.stop()
+        expected_data = [
+            {
+                'status': 'error',
+                'jobId': post_response.DATA['jobId'],
+                'errorMessage': 'Failed to setreuid: Operation not permitted'
+            }
+        ]
+        self.assertEqual(expected_data, webhook_data)
+
+    def test_exception_on_setuid_failure(self):
+        callback_server = self.create_callback_server([200])
+
+        user = '_no_such_user'
+        post_response = self.post(self.jobs_url, {
+            'commandLine': ['true'],
+            'user': user,
+            'callbacks': {
+                'error': callback_server.url,
+            },
+        })
+
+        webhook_data = callback_server.stop()
+        expected_data = [
+            {
+                'status': 'error',
+                'jobId': post_response.DATA['jobId'],
+                'errorMessage': 'getpwnam(): name not found: _no_such_user'
+            }
+        ]
+        self.assertEqual(expected_data, webhook_data)
 
     def _find_match(self, regexp, target):
         match = re.search(regexp, target)
