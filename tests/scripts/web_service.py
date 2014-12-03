61f36912103a089d4510973af99a1457d6d7da58
Mark Burnett <mburnett@genome.wustl.edu>
2014-12-03 14:21:13 -0600

    enable logging for testing

--- tests/scripts/web_service.py
+++ tests/scripts/web_service.py
@@ -1,5 +1,10 @@
 from ptero_lsf.api.wsgi import app
 import os
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 port = int(os.environ['PTERO_LSF_PORT'])
 host = os.environ['PTERO_LSF_HOST']
