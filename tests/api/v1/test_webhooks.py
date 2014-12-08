8f2968b5cde8d04b282b465d26cc661ccc9bcfaf
Mark Burnett <mburnett@genome.wustl.edu>
2014-12-08 09:50:27 -0600

    add tests with both success + failure webhooks

--- tests/api/v1/test_webhooks.py
+++ tests/api/v1/test_webhooks.py
@@ -40,11 +40,10 @@ class TestWebhooks(BaseAPITest):
 
         webhook_data = callback_server.stop()
         pprint.pprint(webhook_data)
-        success_data = webhook_data[-1]
+        data = webhook_data[-1]
 
-        self.assertIsInstance(success_data['lsfJobId'], int)
-        self.assertEqual(success_data['statusHistory'][-1]['status'],
-                'SUCCEEDED')
+        self.assertIsInstance(data['lsfJobId'], int)
+        self.assertEqual(data['statusHistory'][-1]['status'], 'SUCCEEDED')
 
     def test_failure_webhook(self):
         callback_server = self.create_callback_server([200, 200])
@@ -63,8 +62,53 @@ class TestWebhooks(BaseAPITest):
 
         webhook_data = callback_server.stop()
         pprint.pprint(webhook_data)
-        success_data = webhook_data[-1]
+        data = webhook_data[-1]
 
-        self.assertIsInstance(success_data['lsfJobId'], int)
-        self.assertEqual(success_data['statusHistory'][-1]['status'],
-                'FAILED')
+        self.assertIsInstance(data['lsfJobId'], int)
+        self.assertEqual(data['statusHistory'][-1]['status'], 'FAILED')
+
+    def test_success_and_failure_webhook_with_success(self):
+        callback_server = self.create_callback_server([200, 200])
+
+        submit_data = {
+            'command': 'true',
+            'pollingInterval': 30,
+            'webhooks': {
+                'submit': callback_server.url,
+                'success': callback_server.url,
+                'failure': callback_server.url,
+            },
+        }
+        self.set_queue(submit_data)
+
+        post_response = self.post(self.jobs_url, submit_data)
+
+        webhook_data = callback_server.stop()
+        pprint.pprint(webhook_data)
+        data = webhook_data[-1]
+
+        self.assertIsInstance(data['lsfJobId'], int)
+        self.assertEqual(data['statusHistory'][-1]['status'], 'SUCCEEDED')
+
+    def test_success_and_failure_webhook_with_failure(self):
+        callback_server = self.create_callback_server([200, 200])
+
+        submit_data = {
+            'command': 'false',
+            'pollingInterval': 30,
+            'webhooks': {
+                'submit': callback_server.url,
+                'success': callback_server.url,
+                'failure': callback_server.url,
+            },
+        }
+        self.set_queue(submit_data)
+
+        post_response = self.post(self.jobs_url, submit_data)
+
+        webhook_data = callback_server.stop()
+        pprint.pprint(webhook_data)
+        data = webhook_data[-1]
+
+        self.assertIsInstance(data['lsfJobId'], int)
+        self.assertEqual(data['statusHistory'][-1]['status'], 'FAILED')
