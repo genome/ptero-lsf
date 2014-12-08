c31272fcb69ce89e635ebb81194c933742c6106a
Mark Burnett <mburnett@genome.wustl.edu>
2014-12-08 09:44:00 -0600

    test failure webhook

--- tests/api/v1/test_webhooks.py
+++ tests/api/v1/test_webhooks.py
@@ -45,3 +45,26 @@ class TestWebhooks(BaseAPITest):
         self.assertIsInstance(success_data['lsfJobId'], int)
         self.assertEqual(success_data['statusHistory'][-1]['status'],
                 'SUCCEEDED')
+
+    def test_failure_webhook(self):
+        callback_server = self.create_callback_server([200, 200])
+
+        submit_data = {
+            'command': 'false',
+            'pollingInterval': 30,
+            'webhooks': {
+                'submit': callback_server.url,
+                'failure': callback_server.url,
+            },
+        }
+        self.set_queue(submit_data)
+
+        post_response = self.post(self.jobs_url, submit_data)
+
+        webhook_data = callback_server.stop()
+        pprint.pprint(webhook_data)
+        success_data = webhook_data[-1]
+
+        self.assertIsInstance(success_data['lsfJobId'], int)
+        self.assertEqual(success_data['statusHistory'][-1]['status'],
+                'FAILED')
