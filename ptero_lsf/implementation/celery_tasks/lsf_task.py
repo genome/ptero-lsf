14fd8b97720a7107cb3ed42c615fc5524352361e
Mark Burnett <mburnett@genome.wustl.edu>
2014-12-09 08:48:48 -0600

    set error status on job

--- ptero_lsf/implementation/celery_tasks/lsf_task.py
+++ ptero_lsf/implementation/celery_tasks/lsf_task.py
@@ -9,8 +9,17 @@ class LSFTask(celery.Task):
         session = celery.current_app.Session()
         service_job = session.query(models.Job).get(job_id)
 
-        lsf_job = service_job.submit()
+        try:
+            lsf_job = _submit_job(service_job)
 
-        service_job.lsf_job_id = lsf_job.job_id
-        service_job.set_status('SUBMITTED')
+            service_job.lsf_job_id = lsf_job.job_id
+            service_job.set_status('SUBMITTED')
+
+        except:
+            service_job.set_status('ERRORED')
+
         session.commit()
+
+
+def _submit_job(service_job):
+    return service_job.submit()
--- tests/api/v1/test_submit_error.py
+++ tests/api/v1/test_submit_error.py
@@ -1,4 +1,5 @@
 from .base import BaseAPITest
+import pprint
 
 
 class TestSubmitBadRequestShouldReturn400(BaseAPITest):
@@ -31,3 +32,31 @@ class TestSubmitBadRequestShouldReturn400(BaseAPITest):
                 'invalidOption': 'foo',
             },
         })
+
+
+class TestLSFSubmitError(BaseAPITest):
+    def test_invalid_queue(self):
+        callback_server = self.create_callback_server([200])
+
+        submit_data = {
+            'command': 'invalidcommandnamethatcannotbefoundanywhere foo',
+            'options': {
+                'queue': 'thisisareallybogusqueuename',
+            },
+            'webhooks': {
+                'error': callback_server.url,
+                'failure': callback_server.url,
+                'success': callback_server.url,
+            },
+        }
+
+        response = self.post(self.jobs_url, submit_data)
+
+        pprint.pprint(response.headers)
+        pprint.pprint(response.DATA)
+        self.assertEqual(response.status_code, 201)
+
+        webhook_data = callback_server.stop()[0]
+
+        self.assertEqual(webhook_data['statusHistory'][0]['status'], 'NEW')
+        self.assertEqual(webhook_data['statusHistory'][1]['status'], 'ERRORED')
