a21aa9802555f7c1a533994a7fa787bdb79b972d
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-06-30 16:30:21 -0500

    Do not test scheduled and succeeded/failed webhooks together
    The design of the callback webserver is to quit once a fixed number of
    callbacks have been received.  Currently, the scheduled callback may
    occur multiple times while the job is pending (scheduled).
    
    In the future we may enforce that the scheduled webhook is called
    exactly once, and re-introduce these tests.

--- tests/api/v1/test_webhooks.py
+++ tests/api/v1/test_webhooks.py
@@ -23,58 +23,13 @@ class TestWebhooks(BaseAPITest):
         self.assertEqual(webhook_data['statusHistory'][1]['status'],
                 'SUBMITTED')
 
-    def test_success_webhook(self):
-        callback_server = self.create_callback_server([200, 200])
-
-        submit_data = {
-            'command': 'true',
-            'pollingInterval': 30,
-            'webhooks': {
-                'scheduled': callback_server.url,
-                'succeeded': callback_server.url,
-            },
-        }
-        self.update_submit_data(submit_data)
-
-        post_response = self.post(self.jobs_url, submit_data)
-
-        webhook_data = callback_server.stop()
-        pprint.pprint(webhook_data)
-        data = webhook_data[-1]
-
-        self.assertIsInstance(data['lsfJobId'], int)
-        self.assertEqual(data['statusHistory'][-1]['status'], 'SUCCEEDED')
-
-    def test_failure_webhook(self):
-        callback_server = self.create_callback_server([200, 200])
-
-        submit_data = {
-            'command': 'false',
-            'pollingInterval': 30,
-            'webhooks': {
-                'scheduled': callback_server.url,
-                'failed': callback_server.url,
-            },
-        }
-        self.update_submit_data(submit_data)
-
-        post_response = self.post(self.jobs_url, submit_data)
-
-        webhook_data = callback_server.stop()
-        pprint.pprint(webhook_data)
-        data = webhook_data[-1]
-
-        self.assertIsInstance(data['lsfJobId'], int)
-        self.assertEqual(data['statusHistory'][-1]['status'], 'FAILED')
-
     def test_success_and_failure_webhook_with_success(self):
-        callback_server = self.create_callback_server([200, 200])
+        callback_server = self.create_callback_server([200])
 
         submit_data = {
             'command': 'true',
             'pollingInterval': 30,
             'webhooks': {
-                'scheduled': callback_server.url,
                 'succeeded': callback_server.url,
                 'failed': callback_server.url,
             },
@@ -91,13 +46,12 @@ class TestWebhooks(BaseAPITest):
         self.assertEqual(data['statusHistory'][-1]['status'], 'SUCCEEDED')
 
     def test_success_and_failure_webhook_with_failure(self):
-        callback_server = self.create_callback_server([200, 200])
+        callback_server = self.create_callback_server([200])
 
         submit_data = {
             'command': 'false',
             'pollingInterval': 30,
             'webhooks': {
-                'scheduled': callback_server.url,
                 'succeeded': callback_server.url,
                 'failed': callback_server.url,
             },
