0c326681086d9381b56747fd3403a26d0dd14166
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-07-16 15:52:17 -0500

    Revert "Remove SUBMITTED status" and add submitted webhook

--- tests/api/v1/test_webhooks.py
+++ tests/api/v1/test_webhooks.py
@@ -3,13 +3,13 @@ import pprint
 
 
 class TestWebhooks(BaseAPITest):
-    def test_scheduled_webhook(self):
+    def test_submitted_webhook(self):
         callback_server = self.create_callback_server([200])
 
         submit_data = {
             'command': 'true',
             'webhooks': {
-                'scheduled': callback_server.url,
+                'submitted': callback_server.url,
             },
         }
         self.update_submit_data(submit_data)
@@ -21,7 +21,7 @@ class TestWebhooks(BaseAPITest):
         self.assertIsInstance(webhook_data['lsfJobId'], int)
         self.assertEqual(webhook_data['statusHistory'][0]['status'], 'new')
         self.assertEqual(webhook_data['statusHistory'][1]['status'],
-            'scheduled')
+            'submitted')
 
     def test_success_and_failure_webhook_with_success(self):
         callback_server = self.create_callback_server([200])
