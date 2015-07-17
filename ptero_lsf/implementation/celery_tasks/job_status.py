edb957afc3871974b5840c328949a7c23a757431
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-07-17 15:45:02 -0500

    Additional logging

--- ptero_lsf/implementation/celery_tasks/job_status.py
+++ ptero_lsf/implementation/celery_tasks/job_status.py
@@ -1,9 +1,13 @@
 import celery
+import logging
 
 __all__ = ['UpdateJobStatus']
+
+LOG = logging.getLogger(__name__)
 
 
 class UpdateJobStatus(celery.Task):
     def run(self, job_id):
+        LOG.debug('Updating job status for %s' % job_id)
         backend = celery.current_app.factory.create_backend()
         backend.update_job_status(job_id)
--- ptero_lsf/implementation/celery_tasks/polling.py
+++ ptero_lsf/implementation/celery_tasks/polling.py
@@ -1,16 +1,21 @@
 from .. import models
 import celery
 from sqlalchemy import func
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
 class PollActiveJobs(celery.Task):
     def run(self):
+        LOG.debug('Polling active jobs')
         backend = celery.current_app.factory.create_backend()
         session = backend.session
         job_ids = session.query(models.Job.id)\
                 .filter(models.Job.poll_after <= func.now()).all()
 
         for job_id in job_ids:
+            LOG.debug('Scheduling update for job %s' % job_id)
             self.update_job_status.delay(job_id)
 
     @property
