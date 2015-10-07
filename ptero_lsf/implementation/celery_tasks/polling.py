6dea77a675f5a711b5c81f470ec15cf39096a1c8
David Morton <davidlmorton@gmail.com>
2015-10-07 20:13:06 +0000

    Logging for polling task

--- ptero_lsf/implementation/celery_tasks/polling.py
+++ ptero_lsf/implementation/celery_tasks/polling.py
@@ -8,14 +8,14 @@ LOG = logging.getLogger(__name__)
 
 class PollActiveJobs(celery.Task):
     def run(self):
-        LOG.debug('Polling active jobs')
+        LOG.info('Polling DB for active jobs')
         backend = celery.current_app.factory.create_backend()
         session = backend.session
         job_ids = session.query(models.Job.id)\
                 .filter(models.Job.poll_after <= func.now()).all()
 
-        for job_id in job_ids:
-            LOG.debug('Scheduling update for job %s' % job_id)
+        for job_id, in job_ids:
+            LOG.info('Submitting Celery UpdateJobStatus for job (%s)', job_id)
             self.update_job_status.delay(job_id)
         session.commit()
 
