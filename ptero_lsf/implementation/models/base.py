ca825e7183c10522db485cfef1f3a4797c3bdd6b
David Morton <davidlmorton@gmail.com>
2015-10-21 15:33:58 +0000

    Add alembic support

--- ptero_lsf/implementation/models/base.py
+++ ptero_lsf/implementation/models/base.py
@@ -1,7 +1,15 @@
 import sqlalchemy.ext.declarative
+from sqlalchemy import MetaData
 
 
 __all__ = ['Base']
 
+meta = MetaData(naming_convention={
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+})
 
-Base = sqlalchemy.ext.declarative.declarative_base()
+Base = sqlalchemy.ext.declarative.declarative_base(metadata=meta)
