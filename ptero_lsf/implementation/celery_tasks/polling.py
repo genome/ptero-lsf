7b1a56b0689d8ea0008cd6a0de78022ba1310b05
David Morton <davidlmorton@gmail.com>
2015-11-19 19:14:56 +0000

    Move DB related manipulations to backend

--- ptero_lsf/implementation/celery_tasks/polling.py
+++ ptero_lsf/implementation/celery_tasks/polling.py
@@ -1,31 +1,15 @@
-from .. import models
-from sqlalchemy import func
 import celery
 from ptero_common import nicer_logging
-import os
 
 
 LOG = nicer_logging.getLogger(__name__)
-
-MAX_FAILS = os.environ.get("PTERO_LSF_MAX_FAILED_UPDATE_ATTEMPTS", 5)
 
 
 class PollActiveJobs(celery.Task):
     def run(self):
         LOG.info('Polling DB for active jobs')
         backend = celery.current_app.factory.create_backend()
-        session = backend.session
-        jobs = session.query(models.Job).filter(
-                (models.Job.poll_after <= func.now()) &
-                ~(models.Job.awaiting_update) &
-                (models.Job.failed_update_count < MAX_FAILS)
-                ).all()
-
-        job_ids_to_update = []
-        for job in jobs:
-            job_ids_to_update.append(job.id)
-            job.awaiting_update = True
-        session.commit()
+        job_ids_to_update = backend.get_job_ids_to_update()
 
         for job_id in job_ids_to_update:
             LOG.info('Submitting Celery UpdateJobStatus for job (%s)', job_id,
