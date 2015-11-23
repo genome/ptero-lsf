9da65321f8ac3c57a7c83186054d19209e61c3f4
David Morton <davidlmorton@gmail.com>
2015-11-23 18:37:05 +0000

    Add test for canceling job

--- tests/implementation/test_backend.py
+++ tests/implementation/test_backend.py
@@ -1,6 +1,10 @@
+from pprint import pformat
+from ptero_common import statuses
 from ptero_lsf.implementation import Factory
 from ptero_lsf.implementation import models
 import os
+import pwd
+import time
 import unittest
 import uuid
 
@@ -28,3 +32,30 @@ class TestBackend(unittest.TestCase):
         job = self.session.query(models.Job).get(job_id)
         job.lsf_job_id = None
         self.assertFalse(self.backend.update_job_status(job_id))
+
+    def test_create_and_cancel_job(self):
+        job_id = str(uuid.uuid4())
+        user = pwd.getpwuid(os.getuid())[0]
+        print "creating job %s" % job_id
+        self.backend.create_job('sleep 60', job_id, user=user)
+
+        # this should cancel it before it gets dispatched to lsf
+        self.assertFalse(self.backend.job_is_canceled(job_id))
+        print "canceling job %s" % job_id
+        self.backend.cancel_job(job_id, 'Canceled by test')
+        self.assertTrue(self.backend.job_is_canceled(job_id))
+
+        for i in range(20):
+            if self.backend.update_job_status(job_id):
+                print "job %s has dispatched to lsf" % job_id
+                break
+            else:
+                print "Waiting for job %s to dispatch to lsf" % job_id
+                time.sleep(1)
+
+        # the job should be dispatched to lsf and then killed leaving it in a
+        # 'failed' status.
+        job_data = self.backend.get_job(job_id)
+        print "job %s: %s" % (job_id, pformat(job_data))
+        self.assertEqual(self.backend.get_job(job_id)['status'],
+                statuses.failed)
