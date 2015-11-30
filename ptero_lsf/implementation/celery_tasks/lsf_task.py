78f3407bdaa60281b12710cda65c53b3977ace72
David Morton <davidlmorton@gmail.com>
2015-11-30 20:26:59 +0000

    Remove Deprecated LSFTask

--- ptero_lsf/implementation/celery_tasks/lsf_task.py
+++ ptero_lsf/implementation/celery_tasks/lsf_task.py
@@ -5,18 +5,7 @@ from ptero_common import nicer_logging
 LOG = nicer_logging.getLogger(__name__)
 
 
-__all__ = ['LSFTask', 'LSFSubmit', 'LSFKill']
-
-
-class LSFTask(celery.Task):
-    def run(self, job_id):
-        LOG.warning("LSFTask is deprecated, issuing LSFSubmit for job (%s)",
-                job_id, extra={'jobId': job_id})
-
-        new_task = celery.current_app.tasks[
-            'ptero_lsf.implementation.celery_tasks.lsf_task.LSFSubmit'
-        ]
-        new_task.delay(job_id)
+__all__ = ['LSFSubmit', 'LSFKill']
 
 
 class LSFSubmit(celery.Task):
