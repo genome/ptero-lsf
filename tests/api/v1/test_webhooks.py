3561222d7faec626f6a91b23cf98bb897a5d713e
David Morton <davidlmorton@gmail.com>
2015-12-09 06:25:36 +0000

    Support lists of webhooks

--- tests/api/v1/test_webhooks.py
+++ tests/api/v1/test_webhooks.py
@@ -70,3 +70,25 @@ class TestWebhooks(BaseAPITest):
         self.assertIsInstance(data['lsfJobId'], int)
         self.assertIn('jobId', data)
         self.assertEqual(data['statusHistory'][-1]['status'], 'failed')
+
+    def test_multiple_callbacks(self):
+        callback_server = self.create_callback_server([200, 200])
+
+        submit_data = {
+            'command': 'true',
+            'pollingInterval': 30,
+            'webhooks': {
+                'succeeded': [callback_server.url, callback_server.url],
+            },
+        }
+        self.update_submit_data(submit_data)
+
+        self.post(self.jobs_url, submit_data)
+
+        webhook_data = callback_server.stop()
+        pprint.pprint(webhook_data)
+        data = webhook_data[-1]
+
+        self.assertIsInstance(data['lsfJobId'], int)
+        self.assertIn('jobId', data)
+        self.assertEqual(data['statusHistory'][-1]['status'], 'succeeded')
