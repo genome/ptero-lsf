ed2ab53f48f4de4c128736e19c36468e73e36bbb
Michael J. Kiwala <mkiwala@genome.wustl.edu>
2015-12-11 22:12:49 +0000

    Extract alembic config; extract factory classes

--- ptero_lsf/implementation/factory.py
+++ ptero_lsf/implementation/factory.py
@@ -2,9 +2,10 @@ from . import backend
 from . import models  # noqa
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
-from alembic.config import Config
+from ptero_lsf.alembic.config import alembic_config
 from alembic.script import ScriptDirectory
 from alembic import command
+import abc
 import os
 import logging
 
@@ -12,43 +13,34 @@ import logging
 __all__ = ['Factory']
 
 
-class Factory(object):
-    def __init__(self, database_url, celery_app=None):
+class CeleryFactoryMixin(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, celery_app=None):
         self.celery_app = celery_app
-        self.database_url = database_url
-        self._initialized = False
-
-    def create_backend(self):
-        self._initialize()
-        self.db_revision = alembic_db_revision(self.alembic_config)
-        return backend.Backend(session=self.Session(bind=self.engine),
-            celery_app=self.celery_app, db_revision=self.db_revision)
-
-    def _initialize(self):
-        # Lazy initialize to be pre-fork friendly.
-        if not self._initialized:
-            self._initialize_celery()
-            self._initialize_database()
-            self._initialized = True
 
     def _initialize_celery(self):
-        from . import celery_app
-        self.celery_app = celery_app.app
+        if self.celery_app is None:
+            from . import celery_app
+            self.celery_app = celery_app.app
+
+
+class DBFactoryMixin(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, database_url):
+        self.database_url = database_url
+
+    @abc.abstractproperty
+    def base_dir():
+        pass
 
     def _initialize_database(self):
         self.engine = create_engine(self.database_url)
-        self.alembic_config = alembic_config(self.database_url)
+        self.alembic_config = alembic_config(self.base_dir(), self.database_url)
         alembic_upgrade(self.alembic_config, self.engine)
         self.Session = sessionmaker()
         setup_logging()
-
-
-def alembic_config(db_string):
-    alembic_cfg = Config()
-    alembic_cfg.set_main_option('version_locations', versions_dir())
-    alembic_cfg.set_main_option('script_location', scripts_dir())
-    alembic_cfg.set_main_option('url', db_string)
-    return alembic_cfg
 
 
 def alembic_upgrade(alembic_config, engine):
@@ -67,13 +59,32 @@ def setup_logging():
             os.environ.get('PTERO_LSF_ORM_LOG_LEVEL', 'WARN').upper()))
 
 
-def base_dir():
-    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class BigFactory(CeleryFactoryMixin, DBFactoryMixin):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, database_url, celery_app=None):
+        CeleryFactoryMixin.__init__(self, celery_app)
+        DBFactoryMixin.__init__(self, database_url)
+        self._initialized = False
+
+    @abc.abstractproperty
+    def base_dir(self):
+        pass
+
+    def create_backend(self):
+        self._initialize()
+        self.db_revision = alembic_db_revision(self.alembic_config)
+        return backend.Backend(session=self.Session(bind=self.engine),
+            celery_app=self.celery_app, db_revision=self.db_revision)
+
+    def _initialize(self):
+        # Lazy initialize to be pre-fork friendly.
+        if not self._initialized:
+            self._initialize_celery()
+            self._initialize_database()
+            self._initialized = True
 
 
-def versions_dir():
-    return os.path.join(base_dir(), 'alembic', 'versions')
-
-
-def scripts_dir():
-    return os.path.join(base_dir(), 'alembic')
+class Factory(BigFactory):
+    def base_dir(self):
+        return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
